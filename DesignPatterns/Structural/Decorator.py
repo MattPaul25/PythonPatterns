827d9d6df3c33abfc7c106b7bf67ba1bf8488069
unknown <fargusonm@SFO10AC26L1W7.win.dowjones.net>
2015-09-11 16:45:05 -0700

    add'd structural folder

---
+++ DesignPatterns/Structural/Decorator.py
@@ -0,0 +1,34 @@
+#Decorator feature 
+#changes the result of the function by adding on to it (decorates a function)
+#in the below example make_blink is our decorator that alters the result of the hello_world() function
+#from functools import wraps
+
+def make_blink(function):
+    """defines the decorator"""
+
+    #This makes the decorator transparent in terms of its name a nd docstring
+    @wraps(function)
+
+    def decorator():
+        #Grab the return value of the function being decorated
+        ret = function()
+        #Add new functionality to the function being decorated
+        return '<blink>' + ret + '</blink>'
+
+    return decorator
+
+#Apply the decorator here
+@make_blink
+def hello_world():
+    """Original Function""" #these are doc strings
+    return "Hello, World!"
+
+#check the result of decorating
+print(hello_world())
+
+#check if function name is still the same name of the funciton being decorated
+print(hello_world.__name__)
+
+#check if the docstring is still the same as the function being decorated
+print(hello_world.__doc__)
+
---
+++ DesignPatterns/Structural/Proxy.py
@@ -0,0 +1,56 @@
+
+#The word proxy is an oagent or substitute authorized to act for another person or a 
+#document which authorizes the agent so to act
+
+#Proxy is handy when creating a resource intensive object
+#Problem: Postpone object creation unless absolutely necessary
+
+
+#Scenario -create a producer class when he is available (only a fixed number of producers can exist at a current time)
+#Proxy is an artist to see when the producer becomes available
+   #in charge of creating resource heavy object..
+
+
+
+import time
+
+class Producer:
+    """Define the 'resource-intensive' object to instantiate"""
+    def produce(self):
+        print("Producer is working hard")
+
+    def meet(self):
+        print("Producer has time to meet you now!")
+
+class Proxy:
+    """Define the 'realatively less resource-intensive' proxy to instantiate as a middleman"""
+
+    def __init__(self):
+        self.occupied = 'No'
+        self.producer = None
+
+    def produce(self):
+        """check if producer is available"""
+        print('Artist checking if producer is availble....')
+
+        if self.occupied == 'No':
+            #if the producer is available, create a producer object
+            self.producer = Producer()
+            time.sleep(2)
+            #make the producer meet the guest
+            self.producer.meet()
+        else:
+            #otherwise, dont instantiate a producer
+            time.sleep(2)
+            print('Producer is busy..')
+
+           
+#instantiate a proxy 
+p = Proxy()
+
+#make the proxy: artist produce until producer is availble
+p.produce()
+
+p.occupied = 'Yes'
+
+p.produce()
